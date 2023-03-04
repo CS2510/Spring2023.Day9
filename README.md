641d8d27e0019c0999f1e35ef165adeadbd993a1
bricksphd <bricks@unomaha.edu>
2023-03-03 20:12:41 -0600

    Even more comments

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# Spring2023.Day08
+# Spring2023
+
+This directory contains a game engine and several test games for CSCI 2510 at the University of Nebraska at Omaha, Spring 2023.
+
+To run the games, load this directory in a web server (for example LiveServer). Then open the index.html file in each game directory to play the game. Tests can be found in the test.html file in each game directory.
+
+The engine itself can be found in the engine directory. 
--- blink/blink.js
+++ blink/blink.js
@@ -1,10 +1,31 @@
-import * as Engine from "/engine/engine.js"
+//The code for the blink game
 
+//Start by importing the engine
+//To match Unity, we use an import with side effects
+//instead of a traditional import
+import "/engine/engine.js"
+
+/**
+ * The logic behind the blink game.
+ * You can think of this as the controller of the game.
+ * The component tracks the passage of time. 
+ * If up is true, then the passage of time makes the screen
+ * a brighter color of green.
+ * When full green is reach, up becomes false and on
+ * succeeding frames the screen gets darker.
+ * The process reverses when the color reaches black.
+ */
 class BlinkComponent extends Component {
+    /**
+     * Reset all values in the component
+     */
     start() {
         this.time = 0
         this.up = true
     }
+    /**
+     * Update the component by changing the color
+     */
     update() {
         if (this.up) {
             this.time++;
@@ -18,6 +39,10 @@ class BlinkComponent extends Component {
             }
         }
     }
+    /**
+     * Draw the color to the screen.
+     * @param {The drawing context.} ctx 
+     */
     draw(ctx) {
         ctx.fillStyle = `rgb(0, ${this.time} ,0)`
 
@@ -25,13 +50,18 @@ class BlinkComponent extends Component {
     }
 }
 
+/**
+ * Create an instance of the main scene and add the
+ * blink component.
+ */
 let mainScene = new Scene();
-mainScene.start = function(){
-    this.addGameObject(new GameObject("BlinkGameObject").addComponent(new BlinkComponent()))
+mainScene.start = function () {
+
+    this.addGameObject(
+        new GameObject("BlinkGameObject")
+            .addComponent(new BlinkComponent())
+    )
 }
 
-
-
-//SceneManager.addScene(mainScene);
-
+//export the main scene so the .html file can run the game.
 export default mainScene;
